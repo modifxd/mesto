6bb91f6a1fc748a97852b4e321066595385f4b92
modifxd <modifxd@yandex.ru>
2023-08-22 16:09:57 +0300

    2nd iteration

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,14 @@ const popupEditButtonElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = document.querySelector('.popup__close-button');
 const formElement = popupElement.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input-name');
-const jobInput = formElement.querySelector('.popup__input-info');
-const profileInfo = document.querySelector('.profile__subtitle');
+const nameInput = document.getElementById('profile-name');
+const jobInput = document.getElementById('profile-info');
 const profileName = document.querySelector('.profile__title');
+const profileInfo = document.querySelector('.profile__subtitle');
 
 function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileInfo.textContent;
   popupElement.classList.add('popup_opened');
 }
 
